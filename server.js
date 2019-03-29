b85bfa1088d17779db1a82ca2c21b43a108cb523
gmencacci <mencacci_german@hotmail.com>
2019-03-29 15:45:46 -0300

    otra prueba de git 2

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
-
-
-
-//prueba modificar archivo en git
-
-
-
 //Hace público un directorio
 app.use(express.static(__dirname + '/public'));
 
